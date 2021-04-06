afe370385f647a574b40c0bfab15eb57d9564ad2
Frederik Rabøl <frederik-rm@hotmail.com>
2021-04-06 12:05:48 +0200

    added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
 		'plugin:vue/essential',
 		'@vue/airbnb',
 		'@vue/typescript',
+		"prettier",
 	],
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
