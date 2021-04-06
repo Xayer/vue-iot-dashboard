9b3105a01a2c1f48840d863cb7aa879c0d17dc70
Jonas Emil Sommer <jonazzen@icloud.com>
2021-04-06 18:38:27 +0200

    Added todo widget

---
+++ src/components/widgets/todo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="todoApp">
+	<h1>TODO</h1>
+	<p>
+	You have <strong>{{ todos.length }}</strong> tasks on your todo-list.
+	</p>
+	<input
+	v-model="newTodo"
+	v-on:keyup.enter="addTodo"
+	placeholder="What needs to be done?"
+	autofocus
+	/>
+	<ol>
+	<li class="m-b" v-for="todo in todos" :key="todo.id">
+		<input type="checkbox" v-model="todo.done" v-on:click="toggleDoneState(todo)"/>
+		<label :class="{ done: todo.done }">{{ todo.title }}</label>
+		<Button class="danger m-l" v-on:click="removeTodo(todo)">Remove</Button>
+	</li>
+	</ol>
+  </div>
+</template>
+<script>
+import { Button } from '@/components/atoms';
+
+const STORAGE_KEY = 'todos';
+export default {
+	components: { Button },
+	data() {
+		return {
+			newTodo: '',
+			todos: [],
+		};
+	},
+	created() {
+		this.todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+	},
+	methods: {
+		addTodo() {
+			this.todos.push({
+				id: this.todos.length,
+				title: this.newTodo,
+				done: false,
+			});
+			this.newTodo = '';
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+		},
+		toggleDoneState(todo) {
+			this.todos.splice(this.todos.indexOf(todo), 1, {
+				...todo,
+				done: !todo.done,
+			});
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+		},
+		removeTodo(todo) {
+			this.todos.splice(this.todos.indexOf(todo), 1);
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos));
+		},
+	},
+};
+</script>
+<style scoped>
+.done {
+	text-decoration: line-through;
+	color:gray;
+}
+li {
+	list-style-type: none;
+}
+</style>
