4b179e1009a1250bee6094b62b24531467acc6ef
Frederik Rabøl <frederik-rm@hotmail.com>
2021-07-08 19:30:12 +0200

    4cc6072

---
+++ precache-manifest.388ea0f7e2efbb3b0411a9447039c7ad.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb82c5978f3b2a33097a83983c81dc0",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "08426b7276a0cf55a8c6",
+    "url": "/css/app.390597e5.css"
+  },
+  {
+    "revision": "7a1f8af45d9560dddccc",
+    "url": "/css/dashboard.de049543.css"
+  },
+  {
+    "revision": "b1a6517980ca2667f158",
+    "url": "/css/dashboard~edit-dashboard.18400d8d.css"
+  },
+  {
+    "revision": "98182d7ed193f1305984",
+    "url": "/css/edit-dashboard.e58d00ff.css"
+  },
+  {
+    "revision": "c7f7295775e4ac5ba392",
+    "url": "/css/hue-integration.60a94817.css"
+  },
+  {
+    "revision": "a1dc4bae0231454d2d77",
+    "url": "/css/spotify-integration-page.51c71074.css"
+  },
+  {
+    "revision": "fcb1886817140de0f06306e18f47a2a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e5aface09d25a69926",
+    "url": "/js/about.258946b3.js"
+  },
+  {
+    "revision": "08426b7276a0cf55a8c6",
+    "url": "/js/app.971fcf97.js"
+  },
+  {
+    "revision": "fda93f25c770bf79fffd",
+    "url": "/js/chunk-vendors.031265d3.js"
+  },
+  {
+    "revision": "7a1f8af45d9560dddccc",
+    "url": "/js/dashboard.e90b1ce0.js"
+  },
+  {
+    "revision": "b1a6517980ca2667f158",
+    "url": "/js/dashboard~edit-dashboard.1f37c95b.js"
+  },
+  {
+    "revision": "98182d7ed193f1305984",
+    "url": "/js/edit-dashboard.3bce234f.js"
+  },
+  {
+    "revision": "c7f7295775e4ac5ba392",
+    "url": "/js/hue-integration.d5f95965.js"
+  },
+  {
+    "revision": "33cdfdc1b3263882bbca",
+    "url": "/js/integrations.02e72996.js"
+  },
+  {
+    "revision": "a1dc4bae0231454d2d77",
+    "url": "/js/spotify-integration-page.609344ff.js"
+  },
+  {
+    "revision": "0644f8427e703bdf5e790422540e72c1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
