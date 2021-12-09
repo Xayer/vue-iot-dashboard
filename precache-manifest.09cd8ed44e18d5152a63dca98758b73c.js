5e33cbefc5834e1f1f1e0a9ce3aea2d8b470511d
Frederik Rabøl <frederik-rm@hotmail.com>
2021-12-09 21:20:54 +0100

    e2fefd1

---
+++ precache-manifest.09cd8ed44e18d5152a63dca98758b73c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd32f535de5d2274ea87e9fbfa878cb0",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "4c4a9cfd76f3f3603f82",
+    "url": "/css/app.6fa5e2ba.css"
+  },
+  {
+    "revision": "415fb417d7fe2dc9140f",
+    "url": "/css/config-integration.f6156f12.css"
+  },
+  {
+    "revision": "b784fac4b25b665e07b2",
+    "url": "/css/dashboard.4cd9f0c6.css"
+  },
+  {
+    "revision": "beb46d9ac039e6b02784",
+    "url": "/css/dashboard~edit-dashboard.18400d8d.css"
+  },
+  {
+    "revision": "449314ad2b8f6678813e",
+    "url": "/css/edit-dashboard.e58d00ff.css"
+  },
+  {
+    "revision": "7df95237bf3da75f50de",
+    "url": "/css/hue-integration.60a94817.css"
+  },
+  {
+    "revision": "502c2f203ea33bd36b9c",
+    "url": "/css/spotify-integration-page.51c71074.css"
+  },
+  {
+    "revision": "5a78c9f8bbdecba2beabd75c53bb4760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e5aface09d25a69926",
+    "url": "/js/about.258946b3.js"
+  },
+  {
+    "revision": "4c4a9cfd76f3f3603f82",
+    "url": "/js/app.d7a1a829.js"
+  },
+  {
+    "revision": "fda93f25c770bf79fffd",
+    "url": "/js/chunk-vendors.031265d3.js"
+  },
+  {
+    "revision": "415fb417d7fe2dc9140f",
+    "url": "/js/config-integration.50a6b7a8.js"
+  },
+  {
+    "revision": "b784fac4b25b665e07b2",
+    "url": "/js/dashboard.db8a0448.js"
+  },
+  {
+    "revision": "beb46d9ac039e6b02784",
+    "url": "/js/dashboard~edit-dashboard.e9edc377.js"
+  },
+  {
+    "revision": "449314ad2b8f6678813e",
+    "url": "/js/edit-dashboard.40cc57ab.js"
+  },
+  {
+    "revision": "7df95237bf3da75f50de",
+    "url": "/js/hue-integration.de255f12.js"
+  },
+  {
+    "revision": "e2508c21e7b392b95147",
+    "url": "/js/integrations.ccc59c08.js"
+  },
+  {
+    "revision": "502c2f203ea33bd36b9c",
+    "url": "/js/spotify-integration-page.d8f5be7e.js"
+  },
+  {
+    "revision": "0644f8427e703bdf5e790422540e72c1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
